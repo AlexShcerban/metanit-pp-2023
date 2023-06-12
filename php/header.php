d7467bf451fc35894944107169d89274d3af4776
AlexShcerban <108389020+AlexShcerban@users.noreply.github.com>
2023-06-12 18:48:27 +0700

    2
    3 часа инициал
    1 час сейчас

--- php/header.php
+++ php/header.php
@@ -1,6 +1,6 @@
 <?php
     $langid = "1";
-    $name = "c++";  //
+    $nameL = "c++";  //
     $count = 5;
 ?>
 
@@ -9,7 +9,7 @@
     <div id = "header_block">
         <span class = "header_button"><^></span>
         <?php for ($i=0; $i < $count; $i++) { ?>
-            <a href="../html/language.php?lang=<?= $langid ?>" class = "header_button"><?= $name ?></a>
+            <a href="../html/language.php?lang=<?= $langid ?>" class = "header_button"><?= $nameL ?></a>
         <?php } ?>
     </div>
     <div id = "header_button_search">
